82b2777b3729e501ade11cb62e3dcdfc90d5f4a5
Charles J. Geyer <charlie@stat.umn.edu>
2011-02-08 11:22:14 -0600

    version 0.2-1

--- package/foo/R/foo.R
+++ package/foo/R/foo.R
@@ -4,15 +4,15 @@ foo <- function(x, type = c("C", "Fortran", "Call")) {
     stopifnot(is.numeric(x))
 
     if (type == "Fortran") {
-        out <- .Fortran("foo", x = as.double(x), n = length(x))
+        out <- .Fortran("foo", x = as.double(x), n = length(x), PACKAGE = "foo")
         return(out$x)
     }
     if (type == "C") {
-        out <- .C("bar", x = as.double(x), n = length(x))
+        out <- .C("bar", x = as.double(x), n = length(x), PACKAGE = "foo")
         return(out$x)
     }
     if (type == "Call") {
-        return(.Call("baz", x = as.double(x)))
+        return(.Call("baz", x = as.double(x), PACKAGE = "foo"))
     }
 }
 
--- package/foo/tests/foo.R
+++ package/foo/tests/foo.R
@@ -3,7 +3,7 @@
 
  x <- rnorm(10)
 
- x2 <- foo(x)
+ x2 <- foo(x, type = "For")
 
  identical(x^2, x2)
 
